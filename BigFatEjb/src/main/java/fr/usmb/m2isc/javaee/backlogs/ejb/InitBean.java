394cd1e2208710fa7283b334f10f64e1404d0912
ClemiNeko <c.lebras776@laposte.net>
2019-12-09 14:27:14 +0100

    Even more refactoring.

--- BigFatEjb/src/main/java/fr/usmb/m2isc/javaee/backlogs/ejb/InitBean.java
+++ BigFatEjb/src/main/java/fr/usmb/m2isc/javaee/backlogs/ejb/InitBean.java
@@ -32,19 +32,23 @@ public class InitBean implements Init {
         try {
             Backlog annecyBacklog = new Backlog();
             Backlog chamberyBacklog = new Backlog();
+            Backlog grenobleBacklog = new Backlog();
 
             ArrayList<Entry> entriesAnnecy = new ArrayList<Entry>();
             ArrayList<Entry> entriesChambe = new ArrayList<Entry>();
+            ArrayList<Entry> entriesGrenob = new ArrayList<Entry>();
             Entry entry1 = new Entry(simpleDateFormat.parse("20/12/2012"),"Visualiser une carte",45,2,"Pouvoir visualiser la liste des magasins");
             Entry entry2 = new Entry(simpleDateFormat.parse("25/12/2014"),"Ajouter un item",45,2,"Ajouter un item dans le panier");
             Entry entry3 = new Entry(simpleDateFormat.parse("30/12/2017"),"Vider le panier",45,2,"Vider le panier");
             Entry entry4 = new Entry(simpleDateFormat.parse("31/12/2019"),"Reviser JavaEE",45,2,"C'est pas du Kotlin");
+            Entry entry5 = new Entry(simpleDateFormat.parse("15/10/2020"),"Aller sur Mars",45,2,"Elon Musk a enfin finit de s'occuper de ses Tesla.");
+            Entry entry6 = new Entry(simpleDateFormat.parse("20/10/2023"),"Finir le TP",45,2,"On en a fait du chemin");
 
             ArrayList<Comment> commentsEntry1 = new ArrayList<Comment>();
             ArrayList<Comment> commentsEntry2 = new ArrayList<Comment>();
-            Comment comment1 = new Comment("besoin d'aide",bob);
-            Comment comment2 = new Comment("tu te débrouille",stephane);
-            Comment comment3 = new Comment("j'ai bientôt finit :)",stephane);
+            Comment comment1 = new Comment("On a un souci ici bro",bob);
+            Comment comment2 = new Comment("Chacun ses moutons bro",stephane);
+            Comment comment3 = new Comment("On passe a la mise en prod bro :)",stephane);
             commentsEntry1.add(comment1);
             commentsEntry1.add(comment2);
             commentsEntry2.add(comment3);
@@ -59,20 +63,28 @@ public class InitBean implements Init {
             entriesAnnecy.add(entry2);
             entriesAnnecy.add(entry3);
             entriesChambe.add(entry4);
+            entriesGrenob.add(entry5);
+            entriesGrenob.add(entry6);
             annecyBacklog.setEntries(entriesAnnecy);
             chamberyBacklog.setEntries(entriesChambe);
+            grenobleBacklog.setEntries(entriesGrenob);
 
             em.persist(annecyBacklog);
             em.persist(chamberyBacklog);
+            em.persist(grenobleBacklog);
             em.persist(entry1);
             em.persist(entry2);
             em.persist(entry3);
             em.persist(entry4);
+            em.persist(entry5);
+            em.persist(entry6);
 
             Agence annecy = new Agence("Agence Annecy",annecyBacklog);
             Agence chambery = new Agence("Agence Chambery",chamberyBacklog);
+            Agence grenoble = new Agence("Agence Grenoble",grenobleBacklog);
             em.persist(annecy);
             em.persist(chambery);
+            em.persist(grenoble);
 
 
 
